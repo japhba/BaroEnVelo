ffdba00cb1bd1eb914ab9053c3222bdd3439659b
japhba <japhba@users.noreply.github.com>
2017-08-14 12:17:11 +0200

    shows current altitude via the barometer

--- app/src/main/java/com/japhba/baroenvelo/Kontrollzentrum.kt
+++ app/src/main/java/com/japhba/baroenvelo/Kontrollzentrum.kt
@@ -6,11 +6,21 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_kontrollzentrum.*
+import android.widget.Toast
+import android.util.Log
+import android.view.View
+import java.io.IOException
+import java.util.*
+import android.location.LocationManager
+import android.content.Intent
 
-class Kontrollzentrum : AppCompatActivity() {
+class Kontrollzentrum : AppCompatActivity(), SensorEventListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,39 +31,45 @@ class Kontrollzentrum : AppCompatActivity() {
             textView.text = "Heute ist wirklich Sonntag"
         }
 
-        val sensorManager: SensorManager by lazy {
-            getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        }
+        startService(Intent(this, LocationTrackingService::class.java))
+    }
+
+    val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
 
-}
-
-class SensorActivity : Activity(), SensorEventListener {
-    private val mSensorManager: SensorManager
-    private val mAccelerometer: Sensor
-
-    init {
-        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    }
-
-    override protected fun onResume() {
+    override fun onResume() {
         super.onResume()
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),
+                SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 
-    override protected fun onPause() {
+    override fun onPause() {
         super.onPause()
-        mSensorManager.unregisterListener(this)
+        sensorManager.unregisterListener(this)
     }
 
-    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
 
-    override fun onSensorChanged(event: SensorEvent) {
-        val pressure = event.values[0]
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val textView2: TextView = findViewById(R.id.Anzeige)
+
+        val pressure = event!!.values[0]
         val altitude = SensorManager.getAltitude(
                 SensorManager.PRESSURE_STANDARD_ATMOSPHERE, pressure).toString()
-        print(altitude)
+        textView2.text = altitude
+
     }
+
+
+
 }
+
+
+
